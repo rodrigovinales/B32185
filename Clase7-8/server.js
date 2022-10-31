cd1bf0db8f860a05a41b7807fbf3360d71bdae78
rodrigo vinales <rodrigo11@live.com.ar>
2022-10-31 08:49:05 -0300

    Express con Multer / Router

--- Clase7-8/server.js
+++ Clase7-8/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const fs = require('fs')
 const app = express()
+const multer = require('multer')
 const { apiRouter } = require('./apis/apiRouter.js')
 
 
@@ -9,8 +9,40 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/public", express.static('archivos'))
 
+
 // Ruta utilizada
 app.use('/api/productos', apiRouter)
+
+
+
+/// Utilizamos Multer
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
+let storage = multer.diskStorage({
+    destination: function(req, res, cb) {
+        cb(null, __dirname + '/public')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+let subidas = multer({storage: storage})
+
+
+app.post('/uploadfiles', subidas.single('miArchivo'), (req, res, next) => {
+    const file = req.file
+    if(!file) {
+        const error = new Error('Suba un archivo')
+        error.httpStatusCode=400
+        return next(error)
+    }
+    res.send(file)
+})
+
+/////////////////////////////////////////////////////////////////
 
 
 
